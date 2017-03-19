04bd15b3e04bc3fce59b5de6d6311cd94d1e375a
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-03-19 10:11:54 -0700

    restuarant setup done

--- src/com/restaurant/rakshith/Servers.java
+++ src/com/restaurant/rakshith/Servers.java
@@ -1,13 +1,114 @@
 package com.restaurant.rakshith;
 
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * Created by Rakshith on 3/17/2017.
  */
-public class Servers {
+public class Servers implements Cloneable, Comparable<Servers>{
 
+    private int id;
+    private boolean onDuty;
+    private Map<Integer,Table> tablesServed;
+    private double tips;
+    private int addServerIndex;
+
+
+    public Servers(int id, boolean onDuty) {
+        tablesServed=new LinkedHashMap<>();
+        this.id = id;
+        this.onDuty = onDuty;
+    }
+
+
+    public Map<Integer, Table> getTablesServed() {
+        return tablesServed;
+    }
+
+
+    public void setTablesServed(Map<Integer,Table> tablesServed ) {
+        this.tablesServed=tablesServed;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean getOnDuty() {
+        return onDuty;
+    }
+
+    public void setOnDuty(boolean onDuty) {
+        this.onDuty = onDuty;
+    }
+
+    public void serveAnotherTable(Table table){
+        tablesServed.put(table.getId(),table);
+    }
+
+    public void removeTable(Table table) {
+        tablesServed.remove(table.getId());
+    }
+
+    public double getTips() {
+        return tips;
+    }
+
+    public void setTips(double tips) {
+        this.tips = tips;
+    }
 
     @Override
-    public Servers clone() throws CloneNotSupportedException {
-        return (Servers)super.clone();
+    public Servers clone(){
+        try {
+            Servers copy = (Servers)super.clone();
+            copy.tablesServed=new LinkedHashMap<>(tablesServed);
+            return copy;
+        }catch (CloneNotSupportedException e){
+            e.printStackTrace();
+        }
+        return null;
     }
+
+    @Override
+    public int hashCode() {
+        int a = 883;
+        a = id*a*(onDuty?967:1033);
+        a=(tablesServed.hashCode())*a;
+        return a;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj!=null&& getClass()==obj.getClass()){
+             Servers objEqual = (Servers)obj;
+           return onDuty==objEqual.onDuty &&
+                   tips==objEqual.tips &&
+                   tablesServed==objEqual.tablesServed;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+          StringBuilder builder = new StringBuilder();
+                for(Table tableItem : tablesServed.values()){
+                    builder=builder.append(tableItem).append(", ");
+                }
+
+                return "Server id: "+id+" duty status:"
+                +(onDuty?" yes ":" no ") + "Serving Tables : " +builder;
+    }
+
+    public int compareTo(Servers o){
+        return id-o.id;
+    }
+
 }
--- tst/PartyTest.java
+++ tst/PartyTest.java
@@ -13,39 +13,39 @@ public class PartyTest {
 
     @Test
     public void constructorTest() {
+        //Given (Arrange)
         int size = 4;
         String name = "Joe's Party";
 
+        //When(Action)
         Party JoeParty = new Party(name, size);
         JoeParty.setBeingServed(true);
         JoeParty.setSize(size);
         JoeParty.setBeingServed(false);
 
+        // Then (Assertion)
         Assert.assertEquals("Didn't match", size, JoeParty.getSize());
     }
 
 
     @Test
     public void RepeatedNameTest(){
+        //Given (Arrange)
         List<Party> parties = new ArrayList<>();
         List<Table> tables = new ArrayList<>();
 
+        //When(Action)
         Party aParty = new Party("aParty",4);
         Party bParty = new Party("bParty",3);
         Party copyParty = new Party("aParty",4);
-
         parties.add(aParty);
         parties.add(bParty);
         parties.add(copyParty);
-
-
         Table table1 = new Table(1,0);
-
         tables.add(table1);
 
-
-        Assert.assertEquals("Sorry Test failed",aParty.checkRepeatedNames(tables,parties,"aParty"),true);
-
+        // Then (Assertion)
+        Assert.assertEquals("Sorry, Test failed",aParty.checkRepeatedNames(tables,parties,"aParty"),true);
     }
 
 
